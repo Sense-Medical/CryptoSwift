0412d95c0a9631f0a7d434783147020a0f1f85ec
Robert Payne <robertpayne@me.com>
2017-09-13 08:25:06 +1200

    Update Package

--- Package.swift
+++ Package.swift
@@ -1,3 +1,15 @@
+// swift-tools-version:4.0
+
+import Foundation
 import PackageDescription
 
-let package = Package(name: "CryptoSwift")
+let package = Package(
+  name: "CryptoSwift",
+  products: [
+    .library(name: "CryptoSwift", targets: ["CryptoSwift"])
+  ],
+  targets: [
+    .target(name: "CryptoSwift", path: "Sources/CryptoSwift"),
+    .testTarget(name: "CryptoSwiftTests", dependencies: ["CryptoSwift"], path: "Tests/CryptoSwiftTests")
+  ]
+)
